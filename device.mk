8aeb6a909c70af80fc74fed1a39f46a7d4e36f84
Harshit Jain <harshitjain6751@gmail.com>
2021-08-22 16:36:13 +0800

    j4corelte: Strip off some debug packages
    * Ref: https://source.android.com/devices/tech/dalvik/configure#compilation_options
    
    Q: PRODUCT_MINIMIZE_JAVA_DEBUG_INFO wth does this do?
    A: https://android.googlesource.com/platform/build/+/master/target/product/go_defaults_common.mk#68
    
    Change-Id: Iad073ec85f5cfe5be5a8d1944dd7cff521077dbe

--- device.mk
+++ device.mk
@@ -35,9 +35,28 @@ PRODUCT_DEXPREOPT_SPEED_APPS += \
     SystemUI \
     TrebuchetQuickStep
 
+# Disable Scudo outside of eng builds to save RAM.
+ifneq (,$(filter eng, $(TARGET_BUILD_VARIANT)))
+  PRODUCT_DISABLE_SCUDO := true
+endif
+
+# Do not generate libartd.
+PRODUCT_ART_TARGET_INCLUDE_DEBUG_BUILD := false
+
+# Do not spin up a separate process for the network stack on go devices, use an in-process APK.
+PRODUCT_PACKAGES += InProcessNetworkStack
+PRODUCT_PACKAGES += CellBroadcastAppPlatform
+PRODUCT_PACKAGES += CellBroadcastServiceModulePlatform
+PRODUCT_PACKAGES += com.android.tethering.inprocess
+
 # Speed profile services and wifi-service to reduce RAM and storage
 PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
 PRODUCT_ALWAYS_PREOPT_EXTRACTED_APK := true
+
+# Strip the local variable table and the local variable type table to reduce
+# the size of the system image. This has no bearing on stack traces, but will
+# leave less information available via JDWP.
+PRODUCT_MINIMIZE_JAVA_DEBUG_INFO := true
 
 # Shims
 PRODUCT_PACKAGES += \
