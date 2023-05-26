4ee7483a4d64745990a7ba77cd15486c03da8fdd
Vhmit <praxedes.9630@gmail.com>
2023-05-25 22:05:16 -0300

    j4corelte: Inherit proprietary files for j4core
    Signed-off-by: Vhmit <praxedes.9630@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -9,5 +9,16 @@ DEVICE_PATH := device/samsung/j4corelte
 # Inherit from common msm8917-common
 -include device/samsung/msm8917-common/BoardConfigCommon.mk
 
+# Assert
+TARGET_OTA_ASSERT_DEVICE := j4corelte, j4coreltejx
+
+# Kernel
+TARGET_KERNEL_HEADER_ARCH := arm
+TARGET_KERNEL_CONFIG := j4corelte_defconfig
+
+# Partitions
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1644167168
+BOARD_VENDORIMAGE_PARTITION_SIZE := 260046848
+
 # Inherit from the proprietary version
 -include vendor/samsung/j4corelte/BoardConfigVendor.mk
